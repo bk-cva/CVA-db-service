e286ac8f54bc77780bfa95db2fdbbf39590724d0
Vũ Đức Duy <duyisking1060@gmail.com>
2020-04-22 01:01:32 +0700

    Update custom port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 
+const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.useGlobalPipes(new ValidationPipe());
@@ -14,6 +16,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, options);
     SwaggerModule.setup('api', app, document);
 
-    await app.listen(3000);
+    await app.listen(PORT);
 }
 bootstrap();
